bf5a210d33ddb2ccdf619fe29149d87dfc2c04f6
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-10-04 18:59:39 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -480,7 +480,7 @@ else:
                             st.session_state.thumbs_up_states[thumbs_up_key] = True
                             st.session_state.thumbs_down_states.pop(thumbs_up_key, None)
                             # Call save_chat_to_airtable with feedback when thumbs-up is clicked
-                            save_chat_to_airtable(st.session_state.user_name, user_input, output, "👍")
+                            save_chat_to_airtable(st.session_state.user_name, query, answer, "👍")
                     elif thumbs_up_key in st.session_state.thumbs_up_states:
                         st.markdown("👍 You've already given feedback for this message.", unsafe_allow_html=True)
                 
@@ -493,12 +493,11 @@ else:
                             st.session_state.thumbs_down_states[thumbs_down_key] = True
                             st.session_state.thumbs_up_states.pop(thumbs_down_key, None)
                             # Call save_chat_to_airtable with feedback when thumbs-down is clicked
-                            save_chat_to_airtable(st.session_state.user_name, user_input, output, "👎")
+                            save_chat_to_airtable(st.session_state.user_name, query, answer, "👎")
                     elif thumbs_down_key in st.session_state.thumbs_down_states:
                         st.markdown("👎 You've already given feedback for this message.", unsafe_allow_html=True)
-                
+    
                 if feedback is not None:
-                    st.session_state.chat_history[i] = (user_input, output, feedback)
-                    save_chat_to_airtable(st.session_state.user_name, user_input, output, feedback)
+                    st.session_state.chat_history[i] = (query, answer, feedback)
             
                 
