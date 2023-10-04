af6db0ab5e6b5dd11e1537859072c209373e1913
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-10-04 13:10:17 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -457,15 +457,17 @@ else:
                 )
     
             if feedback is None and st.session_state.user_name != "vishakha":
-                # Display thumbs-up and thumbs-down buttons side by side using HTML and CSS
-                st.markdown(
-                    f'<div style="display: flex; justify-content: space-between;">'
-                    f'<button style="padding: 5px; margin: 0px; background-color: #0071bc; color: white; border: none; border-radius: 5px;" onclick="thumbsUp({i})">👍</button>'
-                    f'<button style="padding: 5px; margin: 0px; background-color: #0071bc; color: white; border: none; border-radius: 5px;" onclick="thumbsDown({i})">👎</button>'
-                    f'</div>',
-                    unsafe_allow_html=True
-                )
-            
+                # Display thumbs-up and thumbs-down buttons side by side using columns with reduced spacing
+                thumbs_up_col, thumbs_down_col = st.columns(2)
+                with thumbs_up_col:
+                    thumbs_up = st.button("👍", key=f"thumbs_up_{i}", help="thumbs_up_button",)
+                    if thumbs_up:
+                        feedback = "👍"  # Store thumbs-up feedback
+                with thumbs_down_col:
+                    thumbs_down = st.button("👎", key=f"thumbs_down_{i}", help="thumbs_down_button",)
+                    if thumbs_down:
+                        feedback = "👎"  # Store thumbs-down feedback
+    
                 if feedback is not None:
                     # Update the feedback in the chat history
                     st.session_state.chat_history[i] = (query, answer, feedback)
