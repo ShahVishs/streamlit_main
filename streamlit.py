b0552f2a88caeef41f1dd62a521a7c7dc85be1d1
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-10-30 11:36:26 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -35,57 +35,6 @@ from langchain.agents import AgentExecutor
 from langchain.smith import RunEvalConfig, run_on_dataset
 import pandas as pd
 import json
-
-
-hide_share_button_style = """
-    <style>
-    .st-emotion-cache-zq5wmm.ezrtsby0 .stActionButton:nth-child(1) {
-        display: none !important;
-    }
-    </style>
-"""
-
-hide_star_and_github_style = """
-    <style>
-    .st-emotion-cache-1lb4qcp.e3g6aar1,
-    .st-emotion-cache-30do4w.e3g6aar1 {
-        display: none !important;
-    }
-    </style>
-"""
-
-hide_mainmenu_style = """
-    <style>
-    #MainMenu {
-        display: none !important;
-    }
-    </style>
-"""
-
-hide_fork_app_button_style = """
-    <style>
-    .st-emotion-cache-alurl0.e3g6aar0 {
-        display: none !important;
-    }
-    </style>
-"""
-
-st.markdown(hide_share_button_style, unsafe_allow_html=True)
-st.markdown(hide_star_and_github_style, unsafe_allow_html=True)
-st.markdown(hide_mainmenu_style, unsafe_allow_html=True)
-st.markdown(hide_fork_app_button_style, unsafe_allow_html=True)
-
-# CSS to hide the "Animation Demo" text
-hide_animation_demo_style = """
-    <style>
-    .st-emotion-cache-pkbazv.eczjsme5 {
-        display: none !important;
-    }
-    </style>
-"""
-
-# Apply the CSS styles
-st.markdown(hide_animation_demo_style, unsafe_allow_html=True)
 pd.set_option('display.max_rows', 20)
 pd.set_option('display.max_columns', 20)
 
@@ -295,68 +244,58 @@ else:
     langchain.debug=True
     memory_key = "history"
     memory = AgentTokenBufferMemory(memory_key=memory_key, llm=llm)
-    template = ("""
-    You're the Business Development Manager at a car dealership. 
-    You get text inquiries regarding car inventory, Business details, and scheduling appointments. When responding to inquiries, 
+    template=(
+    """You're the Business Development Manager at a car dealership.
+    You get text enquries regarding car inventory, Business details and scheduling appointments when responding to inquiries,
     strictly adhere to the following guidelines:
-    
+
     Car Inventory Questions: If the customer's inquiry lacks details about make, model, new or used car, and trade-in, 
-    strictly engage by asking for these specific details to better understand the customer's car preferences. 
-    You should know the make of the car, the model of the car, and whether the customer is looking for a new or used car to answer inventory-related queries.
-    When responding to inquiries about any car, restrict the information shared with the customer to the car's make, year, model, and trim. 
-    The selling price should only be disclosed upon the customer's request, without any prior provision of MRP. 
-    If the customer inquires about a car that is not available, please refrain from suggesting other cars. 
-    Provide a link for more details after every car information given.
+    strictly engage by asking for these specific details in order to better understand the customer's car preferences. 
+    You should know make of the car and model of the car, new or used car the costumer is looking for to answer inventory related quries. 
+    When responding to inquiries about any car, restrict the information shared with the customer to the car's make, year, model, and trim.
+    The selling price should only be disclosed upon the customer's request, without any prior provision of MRP.
+    If the customer inquires about a car that is not available, please refrain from suggesting other cars.
+    Provide Link for more details after every car information given.
     
-    Checking Appointments Availability: If the customer's inquiry lacks specific details such as their preferred day, 
-    date, or time, kindly engage by asking for these specifics.
-    {details} Use these details that are today's date and day and find the appointment date from the user's input 
-    and check for appointment availability using the  <a href="https://app.funnelai.com/shorten/JiXfGCEElA" target="_blank">appointment scheduling tool</a> 
-    for that specific day or date and time.
-    
-    For checking appointment availability, you can use a Python Pandas DataFrame. The name of the DataFrame is `df`. 
-    The DataFrame contains data related to appointment schedules. It is important to understand the attributes of the DataFrame before working with it. 
-    This is the result of running `df.head().to_markdown()`. An important rule is to set the option to display all columns without 
-    truncation while using Pandas.
+    Checking Appointments Avaliability: If the customer's inquiry lacks specific details such as their preferred/
+    day, date or time kindly engage by asking for these specifics.
+    {details} Use these details that is todays date and day and find the appointment date from the users input
+    and check for appointment availabity using python_repl function mentioned in the tools for 
+    that specific day or date and time.
+    For checking appointment vailability you use pandas dataframe in Python. The name of the dataframe is `df`. The dataframe contains 
+    data related appointment schedule. It is important to understand the attributes of the dataframe before working with it. 
+    This is the result of running `df.head().to_markdown()`. Important rule is set the option to display all columns without
+    truncation while using pandas.
     <df>
     {dhead}
     </df>
-    
-    You are not meant to use only these rows to answer questions; they are meant as a way of telling you 
-    about the shape and schema of the DataFrame. You can run intermediate queries to do exploratory data analysis to give you more information as needed.
-    
-    If the appointment schedule time is not available for the specified date and time, 
-    you can provide alternative available times near the customer's preferred time from the information given to you. 
-    In the answer, use AM and PM time formats; strictly don't use the 24-hour format. 
-    Additionally, provide this <a href="https://app.funnelai.com/shorten/JiXfGCEElA" target="_blank">link</a> for scheduling an appointment by the user himself.
-    Prior to scheduling an appointment, please commence a conversation by soliciting the following customer information: 
+    You are not meant to use only these rows to answer questions - they are meant as a way of telling you
+    about the shape and schema of the dataframe. You can run intermediate queries to do exploratory data analysis to give you more information as needed.
+
+    If the appointment schedule time is not available for the specified 
+    date and time you can provide alternative available times near to costumer's preferred time from the information given to you.
+    In answer use AM, PM time format strictly don't use 24 hrs format.
+    Additionally provide this link: https://app.funnelai.com/shorten/JiXfGCEElA to schedule appointment by the user himself.
+    Prior to scheduling an appointment, please commence a conversation by soliciting the following customer information:
     their name, contact number, and email address.
-    
-    Business Details: Inquiry regarding Google Maps location of the store, address of the store, working days and working hours, 
-    and contact details. Use the `search_business_details` tool to get information.
-    
-    Encourage Dealership Visit: Our goal is to encourage customers to visit the dealership for test drives or 
-    receive product briefings from our team. After providing essential information on the car's make, model, 
-    color, and basic features, kindly invite the customer to schedule an appointment for a test drive or visit us 
+
+    Business details: Enquiry regarding google maps location of the store, address of the store, working days and working hours 
+    and contact details use search_business_details tool to get information.
+
+    Encourage Dealership Visit: Our goal is to encourage customers to visit the dealership for test drives or
+    receive product briefings from our team. After providing essential information on the car's make, model,
+    color, and basic features, kindly invite the customer to schedule an appointment for a test drive or visit us
     for a comprehensive product overview by our experts.
-    
+
     Please maintain a courteous and respectful tone in your American English responses./
     If you're unsure of an answer, respond with 'I am sorry.'/
-    Make every effort to assist the customer promptly while keeping responses concise, not exceeding two sentences.
-    Very Very Important Instruction: whenever you are using tools to answer the question, strictly answer only from the "System: " message provided to you.
-    """)
-       
-    # # Find the position of "here" in the template
-    # start_pos = template.find("<a")
-    # end_pos = template.find("</a>") + len("</a>")
-    
-    # # Extract the clickable link part
-    # clickable_link = template[start_pos:end_pos]
-    
-    # # Display only the clickable link
-    # st.markdown(clickable_link, unsafe_allow_html=True)
+    Make every effort to assist the customer promptly while keeping responses concise, not exceeding two sentences."
+
+    Very Very Important Instruction: whenever you are using tools to answer the question. 
+    strictly answer only from the "System: " message provided to you.""")
+
     details= "Today's current date is "+ todays_date +" today's weekday is "+day_of_the_week+"."
-    
+
     class PythonInputs(BaseModel):
         query: str = Field(description="code snippet to run")
 
@@ -389,8 +328,8 @@ else:
 
     airtable = Airtable(AIRTABLE_BASE_ID, AIRTABLE_TABLE_NAME, api_key=airtable_api_key)
 
-    # Function to save chat history with feedback to Airtable
-    def save_chat_to_airtable(user_name, user_input, output, feedback):
+    # Function to save chat history to Airtable
+    def save_chat_to_airtable(user_name, user_input, output):
         try:
             timestamp = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
             airtable.insert(
@@ -399,25 +338,23 @@ else:
                     "question": user_input,
                     "answer": output,
                     "timestamp": timestamp,
-                    "feedback": feedback if feedback is not None else ""  # Store an empty string if feedback is None
                 }
             )
-            print(f"Data saved to Airtable - User: {user_name}, Question: {user_input}, Answer: {output}, Feedback: {feedback}")
         except Exception as e:
             st.error(f"An error occurred while saving data to Airtable: {e}")
     # Initialize session state
     if 'chat_history' not in st.session_state:
         st.session_state.chat_history = []
     # Function to perform conversational chat
-    # Function to perform conversational chat
     def conversational_chat(user_input):
-        for query, answer, feedback in reversed(st.session_state.chat_history):
-            if query.lower() == user_input.lower():
-                return answer, feedback if feedback else None  # Return None if feedback is not available
+        for query, answer in reversed(st.session_state.chat_history):
+            if query.lower() == user_input.lower():  
+                return answer
         result = agent_executor({"input": user_input})
+        # st.session_state.chat_history.append((user_input, result["output"]))
+        # return result["output"]
         response = result["output"]
-        feedback = None  # Initialize feedback as None
-        return response, feedback
+        return response
     if st.session_state.user_name is None:
         user_name = st.text_input("Your name:")
         if user_name:
@@ -430,84 +367,49 @@ else:
             st.session_state.new_session = False  # Prevent clearing chat history
             st.session_state.sessions = load_previous_sessions()
             
-    
-    # User input and chat history display
-    # User input and chat history display
-    user_input = ""
+    user_input = ""   
     output = ""
-    feedback = None  # Initialize feedback as None
     
+    # if st.session_state.user_name is None:
+        
+    #     user_name = st.text_input("Your name:")
+    #     if user_name:
+    #         st.session_state.user_name = user_name
+    #     if user_name == "vishakha":
+    #         is_admin = True
+    #         st.session_state.user_role = "admin"
+    #         st.session_state.user_name = user_name
+    #         st.session_state.new_session = False 
+    #         st.session_state.sessions = load_previous_sessions()
+
     with st.form(key='my_form', clear_on_submit=True):
         if st.session_state.user_name != "vishakha":
             user_input = st.text_input("Query:", placeholder="Type your question here :)", key='input')
         submit_button = st.form_submit_button(label='Send')
-    
+
     if submit_button and user_input:
-        output, feedback = conversational_chat(user_input)
-        st.session_state.chat_history.append((user_input, output, feedback))  # Store feedback along with response
-        print(f"Data to be saved - User: {st.session_state.user_name}, Question: {user_input}, Answer: {output}, Feedback: {feedback}")
-        save_chat_to_airtable(st.session_state.user_name, user_input, output, feedback)
-    
-    # Display chat history with feedback
-    # Add this line to initialize chat_history
-    if 'chat_history' not in st.session_state:
-        st.session_state.chat_history = []
+        output = conversational_chat(user_input)
+        st.session_state.chat_history.append((user_input, output))
 
-   # Initialize thumbs_up_states and thumbs_down_states in session state
-    if 'thumbs_up_states' not in st.session_state:
-        st.session_state.thumbs_up_states = {}
-
-    if 'thumbs_down_states' not in st.session_state:
-        st.session_state.thumbs_down_states = {}
-
-
-    # Display chat history with feedback
-        # Display chat history with feedback
-    # Display chat history with feedback
     with response_container:
-        for i, (query, answer, feedback) in enumerate(st.session_state.chat_history):
+        for i, (query, answer) in enumerate(st.session_state.chat_history):
             user_name = st.session_state.user_name
-            message(query, is_user=True, key=f"{i}_user", avatar_style="thumbs")
-            col1, col2 = st.columns([0.7, 10])
+            message(query, is_user=True, key=f"{i}_user", avatar_style="big-smile")
+            col1, col2 = st.columns([0.7, 10]) 
             with col1:
                 st.image("icon-1024.png", width=50)
             with col2:
                 st.markdown(
-                    f'<div style="background-color: black; color: white; border-radius: 10px; padding: 10px; width: 60%;'
-                    f' border-top-right-radius: 10px; border-bottom-right-radius: 10px;'
-                    f' border-top-left-radius: 0; border-bottom-left-radius: 0; box-shadow: 2px 2px 5px #888888;">'
-                    f'<span style="font-family: Arial, sans-serif; font-size: 16px; white-space: pre-wrap;">{answer}</span>'
-                    f'</div>',
-                    unsafe_allow_html=True
+                f'<div style="background-color: #F5F5F5; border-radius: 10px; padding: 10px; width: 50%;'
+                f' border-top-right-radius: 10px; border-bottom-right-radius: 10px;'
+                f' border-top-left-radius: 0; border-bottom-left-radius: 0; box-shadow: 2px 2px 5px #888888;">'
+                f'<span style="font-family: Arial, sans-serif; font-size: 16px; white-space: pre-wrap;">{answer}</span>'
+                f'</div>',
+                unsafe_allow_html=True
                 )
-    
-            if feedback is None and st.session_state.user_name != "vishakha":
-                # Display thumbs-up and thumbs-down buttons side by side using columns with reduced spacing
-                thumbs_up_col, thumbs_down_col = st.columns(2)
-                with thumbs_up_col:
-                    thumbs_up_key = f"thumbs_up_{i}"
-                    if thumbs_up_key not in st.session_state.thumbs_up_states or not st.session_state.thumbs_up_states[thumbs_up_key]:
-                        thumbs_up = st.button("👍", key=thumbs_up_key, help="thumbs_up_button",)
-                        if thumbs_up:
-                            st.session_state.thumbs_up_states[thumbs_up_key] = True
-                            st.session_state.thumbs_down_states.pop(thumbs_up_key, None)
-                            # Call save_chat_to_airtable with feedback when thumbs-up is clicked
-                            save_chat_to_airtable(st.session_state.user_name, query, answer, "👍")
-                    elif thumbs_up_key in st.session_state.thumbs_up_states:
-                        st.markdown("👍", unsafe_allow_html=True)
-                
-                # Display thumbs-down button conditionally based on its state
-                with thumbs_down_col:
-                    thumbs_down_key = f"thumbs_down_{i}"
-                    if thumbs_down_key not in st.session_state.thumbs_down_states or not st.session_state.thumbs_down_states[thumbs_down_key]:
-                        thumbs_down = st.button("👎", key=thumbs_down_key, help="thumbs_down_button",)
-                        if thumbs_down:
-                            st.session_state.thumbs_down_states[thumbs_down_key] = True
-                            st.session_state.thumbs_up_states.pop(thumbs_down_key, None)
-                            # Call save_chat_to_airtable with feedback when thumbs-down is clicked
-                            save_chat_to_airtable(st.session_state.user_name, query, answer, "👎")
-                    elif thumbs_down_key in st.session_state.thumbs_down_states:
-                        st.markdown("👎", unsafe_allow_html=True)
-    
-                if feedback is not None:
-                    st.session_state.chat_history[i] = (query, answer, feedback)
+
+        if st.session_state.user_name and st.session_state.chat_history:
+            try:
+                save_chat_to_airtable(st.session_state.user_name, user_input, output)
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
