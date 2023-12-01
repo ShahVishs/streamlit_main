342c348ee7437d348323fa0339d28ef587270d29
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-12-01 13:45:59 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -335,8 +335,8 @@ else:
         if 'chat_history' not in st.session_state or not st.session_state.chat_history:
             # If chat history is empty, just return without saving to Airtable
             return
-        
-        filtered_chat_history = [(query, answer, feedback, source) for query, answer, feedback, source in st.session_state.chat_history if query is not None and answer is not None and feedback is not None]
+        filtered_chat_history = [(query, answer, feedback, source) for query, answer, feedback, source in st.session_state.chat_history if query is not None and answer is not None and feedback is not None] if len(st.session_state.chat_history[0]) == 4 else [(query, answer, None, source) for query, answer, _, source in st.session_state.chat_history if query is not None and answer is not None]
+        # filtered_chat_history = [(query, answer, feedback, source) for query, answer, feedback, source in st.session_state.chat_history if query is not None and answer is not None and feedback is not None]
         complete_conversation = "\n".join([f"user:{query}\nAI:{answer}\nFeedback:{feedback}" for query, answer, feedback, _ in st.session_state.chat_history])
     
         try:
