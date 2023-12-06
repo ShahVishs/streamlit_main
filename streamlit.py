fea0a2f5659980b8be0dea9642bd2ec2daba20de
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-12-06 13:39:44 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -294,11 +294,11 @@ else:
            If more options match, convey that several models are available.
         Remember: Keep responses concise and only share selling prices when requested but don't answer be like If you would like to know the selling price .
         Appointment Scheduling:
-        1. After gathering Make, Model, and New/Used info from the customer, provide car details only when the model and new or used car details are available.
-        2. For appointments, check availability using our appointment schedule data, stored in the `df` dataframe. If no specific details are provided in the inquiry, engage with the customer to ascertain their preferences.
-        3. Before scheduling, inquire about car trade-in. Ask for name, contact number, and email separately.
-        4. If they ask for rescheduling, then provide the answer by checking the availability and also provide a link:
-           [link for Reschedule Appointment](https://app.funnelai.com/shorten/JiXfGCEElA)
+        After gathering Make, Model, and New/Used info from the customer, provide car details only when the model and new or used car details are available. 
+        For appointments, check availability using our appointment schedule data, stored in the `df` dataframe. If no specific details are provided in the inquiry, engage with the customer to ascertain their preferences.
+        Before scheduling, inquire about car trade-in. Ask for name, contact number, and email separately. 
+        If they ask for resheduling then provide the answer by checking the availability and also provide a following link:
+        [Link for Reschedule Appointment](https://app.funnelai.com/shorten/JiXfGCEElA)
         Keep responses concise and assist the customers promptly.""")
         
 
