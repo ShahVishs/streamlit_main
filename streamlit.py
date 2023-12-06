e43c92982687ee47fb7c365d5f24a4f7e8b16ba3
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-12-06 13:47:59 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -297,8 +297,6 @@ else:
         After gathering Make, Model, and New/Used info from the customer, provide car details only when the model and new or used car details are available. 
         For appointments, check availability using our appointment schedule data, stored in the `df` dataframe. If no specific details are provided in the inquiry, engage with the customer to ascertain their preferences.
         Before scheduling, inquire about car trade-in. Ask for name, contact number, and email separately. 
-        If they ask for resheduling then provide the answer by checking the availability and also provide a following link:
-        [Link for Reschedule Appointment](https://app.funnelai.com/shorten/JiXfGCEElA)
         Keep responses concise and assist the customers promptly.""")
         
 
