d45552cf11d26ee1324834f601945480a1741f28
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-12-07 16:28:56 +0530

    Update streamlit_new.py

--- streamlit_new.py
+++ streamlit_new.py
@@ -521,10 +521,11 @@ with st.form(key='feedback_form'):
 _RELEASE = False
 
 if not _RELEASE:
-    _component_func = components.declare_component(
-        "st_autorefresh",
-        url="http://localhost:3001",
-    )
+    pass
+    # _component_func = components.declare_component(
+    #     "st_autorefresh",
+    #     url="http://localhost:3001",
+    # )
 else:
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(parent_dir, "frontend/build")
