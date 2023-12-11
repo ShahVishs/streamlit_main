1f176bf481476f7e619024a0d0d282440019f479
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2023-12-11 16:15:56 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -558,21 +558,30 @@ else:
     
             with col2:
                 # Use another column for the actual response content
+                container_style = (
+                    'background-color: black; color: white; border-radius: 0; padding: 5px; width: 100%;'
+                    ' box-shadow: 2px 2px 5px #888888;'
+                )
+                
+                if "website Link for images" in answer:
+                    # If the response contains an image, set a wider width
+                    container_style = (
+                        'background-color: black; color: white; border-radius: 0; padding: 5px; width: 100%;'
+                        ' box-shadow: 2px 2px 5px #888888;'
+                    )
+                else:
+                    # If there is no image, set a narrower width
+                    container_style = (
+                        'background-color: black; color: white; border-radius: 0; padding: 5px; width: 70%;'
+                        ' box-shadow: 2px 2px 5px #888888;'
+                    )
+                
                 st.markdown(
-                    f'<div style="background-color: black; color: white; border-radius: 0; padding: 5px; width: 100%;'
-                    f' box-shadow: 2px 2px 5px #888888;">'
+                    f'<div style="{container_style}">'
                     f'<span style="font-family: Arial, sans-serif; font-size: 16px; white-space: pre-wrap;">{answer}</span>'
                     f'</div>',
                     unsafe_allow_html=True
                 )
-    
-                # Extracting image URL from the answer and displaying the image
-                if "website Link for images" in answer:
-                    image_urls = answer["website Link for images"]
-    
-                    # Display images separately
-                    for image_url in image_urls:
-                        resize_and_display_image(image_url, max_width=50)  # Adjust max_width as needed
             if feedback is None and st.session_state.user_name != "vishakha":
                 thumbs_up_col, thumbs_down_col = st.columns(2)
                 with thumbs_up_col:
