cf91d98649cd962786a6e13fe02789c23470c784
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2024-01-03 16:05:37 +0530

    Update streamlit_p.py

--- streamlit_p.py
+++ streamlit_p.py
@@ -95,7 +95,7 @@ business_details_text = [
 ]
 retriever_3 = FAISS.from_texts(business_details_text, OpenAIEmbeddings()).as_retriever()
 
-file_1 = r'car_desription_new.csv'
+file_1 = r'inventory_goush_cleaned_new.csv'
 
 loader = CSVLoader(file_path=file_1)
 docs_1 = loader.load()
