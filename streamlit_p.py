2772dd07f634438cc79c415686832afff97e7ba1
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2024-01-03 23:49:22 +0530

    Update streamlit_p.py

--- streamlit_p.py
+++ streamlit_p.py
@@ -477,9 +477,9 @@ with container:
         
         # Display images in Streamlit
         for car_info in car_info_list:
-            st.image(car_info["website Link for images"], caption=f"{car_info['year']} {car_info['make']} {car_info['model']}")
-            st.write(f"VIN: {car_info['vin']}")
-            st.write(f"Link: {car_info['link']}")
+            st.image(car_info["website Link for images"], caption=f"{car_info['Year']} {car_info['Make']} {car_info['Model']}")
+            st.write(f"VIN: {car_info['Vin']}")
+            st.write(f"Link: {car_info['Link']}")
     with response_container:
         for i, (query, answer) in enumerate(st.session_state.chat_history):
             message(query, is_user=True, key=f"{i}_user", avatar_style="thumbs")
