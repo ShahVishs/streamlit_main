c54b0ae0514724877d8da3b3f5ff318f6f23e79e
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2024-01-04 14:33:40 +0530

    Update streamlit_p.py

--- streamlit_p.py
+++ streamlit_p.py
@@ -559,8 +559,8 @@ with container:
                         image = Image.open(BytesIO(image_response.content))
                         
                         # Resize the image to a smaller size
-                        width = 150
-                        height = 100
+                        width = 250
+                        height = 150
                         resized_image = image.resize((width, height))
                         
                         # Display the resized image
