7dbbbf1df77b8182785e90cd3bfe98670f6794dc
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2024-02-08 17:19:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -571,11 +571,11 @@ if template is not None:
                                    return_source_documents=True, return_generated_question=True)
     st.session_state.agent_executor = agent_executor
     
-# chat_history=[]
-chat_history = st.session_state.chat_history
-response_container = st.container()
-container = st.container()
-airtable = Airtable(AIRTABLE_BASE_ID, AIRTABLE_TABLE_NAME, api_key=airtable_api_key)
+    chat_history=[]
+    # chat_history = st.session_state.chat_history
+    response_container = st.container()
+    container = st.container()
+    airtable = Airtable(AIRTABLE_BASE_ID, AIRTABLE_TABLE_NAME, api_key=airtable_api_key)
 
 
 if 'chat_history' not in st.session_state:
