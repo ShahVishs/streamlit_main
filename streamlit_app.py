cb2e7f9d7cb4395d1d633623ad847403144c6996
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2024-02-09 12:06:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -551,7 +551,6 @@ def get_template(response_style):
     Use this tool "store_appointment_data" to store the data.
     If any of the above details missing you can enquire about that."""
 
-# Check if response_style is in session state, if not set a default
 if 'response_style' not in st.session_state:
     st.session_state.response_style = "Humorous"  # Set a default response style
 
@@ -587,13 +586,8 @@ prompt = OpenAIFunctionsAgent.create_prompt(
 )
 tools = [tool1, tool2, tool3, get_car_details_from_vin, get_appointment_details, store_appointment_data]
 agent = OpenAIFunctionsAgent(llm=llm, tools=tools, prompt=prompt)
-# agent_executor = AgentExecutor(agent=agent, tools=tools, memory=memory, verbose=True, return_source_documents=True,
-#         return_generated_question=True)
-# st.session_state.agent_executor = agent_executor
 
-# Check if agent_executor is not in session state or if the response_style has changed
 if 'agent_executor' not in st.session_state or st.session_state.agent_executor.response_style != response_style:
-    agent = OpenAIFunctionsAgent(llm=llm, tools=tools, prompt=prompt)
     agent_executor = AgentExecutor(agent=agent, tools=tools, memory=memory, verbose=True, return_source_documents=True,
             return_generated_question=True, response_style=response_style)
     st.session_state.agent_executor = agent_executor
@@ -605,13 +599,12 @@ if 'chat_history' not in st.session_state:
     st.session_state.chat_history = []
 
 chat_history = st.session_state.chat_history
-
 response_container = st.container()
 container = st.container()
 airtable = Airtable(AIRTABLE_BASE_ID, AIRTABLE_TABLE_NAME, api_key=airtable_api_key)
 
-if 'chat_history' not in st.session_state:
-    st.session_state.chat_history = []
+# if 'chat_history' not in st.session_state:
+#     st.session_state.chat_history = []
 
 if 'user_name' not in st.session_state:
     st.session_state.user_name = None
