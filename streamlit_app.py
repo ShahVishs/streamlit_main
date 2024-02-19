8c0053df93a07e7ef7416ca7479f6a3f888d53a2
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2024-02-19 14:46:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -651,6 +651,20 @@ def conversational_chat(user_input, user_name):
     
     return output
 
+# def convert_text_to_html_images(text):
+#     # Pattern to match the specific format
+#     pattern = r"image_url:([^,]+), car_details_url:([^,\s]+)"
+    
+#     # Function to replace each match with an HTML string
+#     def replace_with_html(match):
+#         image_url = match.group(1).strip()
+#         car_details_url = match.group(2).strip()
+#         return f'<a href="{car_details_url}"><img src="{image_url}" alt="Car Image" style="width:100px;height:auto;"/></a>'
+    
+#     # Replace all occurrences in the text
+#     html_text = re.sub(pattern, replace_with_html, text)
+#     return html_text
+
 def convert_text_to_html_images(text):
     # Pattern to match the specific format
     pattern = r"image_url:([^,]+), car_details_url:([^,\s]+)"
@@ -659,7 +673,7 @@ def convert_text_to_html_images(text):
     def replace_with_html(match):
         image_url = match.group(1).strip()
         car_details_url = match.group(2).strip()
-        return f'<a href="{car_details_url}"><img src="{image_url}" alt="Car Image" style="width:100px;height:auto;"/></a>'
+        return f'<a href="{car_details_url}" target="_blank"><img src="{image_url}" alt="Car Image" style="width:100px;height:auto;"/></a>'
     
     # Replace all occurrences in the text
     html_text = re.sub(pattern, replace_with_html, text)
