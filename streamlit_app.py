d52ab941003791e29cbc1e723c0dfd60085b000b
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2024-02-19 22:37:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -607,19 +607,18 @@ def convert_text_to_html_images(text):
     
 def convert_links(text):
     # Regular expression to match markdown format ![alt text](URL) or [link text](URL)
-    pattern = r'!?\[([^\]]+)\]\(([^)]+)\)'
+    pattern = r'<a\s*href="([^"]*)"\s*target="_blank">([^<]*)</a>'
 
     # Function to replace each match
     def replace_with_tag(match):
-        prefix = match.group(0)[0]  # Check if it's an image or a link
-        alt_or_text = match.group(1)
-        url = match.group(2)
-
+        url = match.group(1)
+        text = match.group(2)
+        
         # Check for common image file extensions
         if any(url.lower().endswith(ext) for ext in ['.jpg', '.jpeg', '.png', '.gif']):
-            return f'<img src="{url}" alt="{alt_or_text}" style="width: 100px; height: auto;"/>'
+            return f'<a href="{url}" target="_blank"><img src="{url}" alt="{text}" style="width: 100px; height: auto;"/></a>'
         else:
-            return f'<a href="{url}" target="_blank">{alt_or_text}</a>'
+            return f'<a href="{url}" target="_blank">{text}</a>'
 
     # Replace all occurrences
     html_text = re.sub(pattern, replace_with_tag, text, flags=re.DOTALL)  # Add re.DOTALL to handle newlines in the pattern
@@ -645,18 +644,11 @@ with container:
             with col1:
                 st.image("icon-1024.png", width=50)
             with col2:
-                # Extract URLs from the answer
-                urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', answer)
-                
-                # Add redirection links for each URL
-                for url in urls:
-                    answer = answer.replace(url, f'{convert_links(url)}')  # Use convert_links to handle the URL
-                    
                 st.markdown(
                     f'<div style="background-color: black; color: white; border-radius: 10px; padding: 10px; width: 85%;'
                     f' border-top-right-radius: 10px; border-bottom-right-radius: 10px;'
                     f' border-top-left-radius: 0; border-bottom-left-radius: 0; box-shadow: 2px 2px 5px #888888;">'
-                    f'<span style="font-family: Arial, sans-serif; font-size: 16px; white-space: pre-wrap;">{answer}</span>'
+                    f'<span style="font-family: Arial, sans-serif; font-size: 16px; white-space: pre-wrap;">{convert_links(answer)}</span>'
                     f'</div>',
                     unsafe_allow_html=True
                 )
