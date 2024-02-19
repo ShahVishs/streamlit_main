efddf2b46ce37886da41555fb25e0ccfa8d2b419
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2024-02-19 14:11:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -687,6 +687,19 @@ def conversational_chat(user_input, user_name):
 #     html_text = re.sub(pattern, replace_with_tag, text)
 
 #     return html_text    
+# def convert_text_to_html_images(text):
+#     # Pattern to match the specific format
+#     pattern = r"image_url:([^,]+), car_details_url:([^,\s]+)"
+    
+#     # Function to replace each match with an HTML string
+#     def replace_with_html(match):
+#         image_url = match.group(1).strip()
+#         car_details_url = match.group(2).strip()
+#         return f'<a href="{car_details_url}" target="_blank"><img src="{image_url}" alt="Car Image" style="width:100px;height:auto; display:block; margin:auto;"/></a>'
+    
+#     # Replace all occurrences in the text
+#     html_text = re.sub(pattern, replace_with_html, text)
+#     return html_text
 def convert_text_to_html_images(text):
     # Pattern to match the specific format
     pattern = r"image_url:([^,]+), car_details_url:([^,\s]+)"
@@ -695,12 +708,11 @@ def convert_text_to_html_images(text):
     def replace_with_html(match):
         image_url = match.group(1).strip()
         car_details_url = match.group(2).strip()
-        return f'<a href="{car_details_url}" target="_blank"><img src="{image_url}" alt="Car Image" style="width:100px;height:auto; display:block; margin:auto;"/></a>'
+        return f'<div><a href="{car_details_url}" target="_blank"><img src="{image_url}" alt="Car Image" style="width:100px;height:auto; display:block; margin:auto;"/></a></div>'
     
     # Replace all occurrences in the text
     html_text = re.sub(pattern, replace_with_html, text)
     return html_text
-
 def convert_links(text):
     # Regular expression to match markdown format ![alt text](URL) or [link text](URL)
     pattern = r'!?\[([^\]]+)\]\(([^)]+)\)'
