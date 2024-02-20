4f101d447046ea32ebf2c02f8a4ec221e62bae9e
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2024-02-20 16:30:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -781,7 +781,7 @@ with container:
                     f'<div style="background-color: black; color: white; border-radius: 10px; padding: 10px; width: 85%;'
                     f' border-top-right-radius: 10px; border-bottom-right-radius: 10px;'
                     f' border-top-left-radius: 0; border-bottom-left-radius: 0; box-shadow: 2px 2px 5px #888888;">'
-                    f'<span style="font-family: Arial, sans-serif; font-size: 16px; white-space: pre-wrap;">{convert_links(answer, extract_details_urls(output))}</span>'
+                    f'<span style="font-family: Arial, sans-serif; font-size: 16px; white-space: pre-wrap;">{convert_links(answer, details_urls)}</span>'
                     f'</div>',
                     unsafe_allow_html=True
                 )
