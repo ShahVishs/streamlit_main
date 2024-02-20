8382b324487d9a081d3dafe5884b35ea09a4e474
ShahVishs <121416561+ShahVishs@users.noreply.github.com>
2024-02-20 16:52:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -751,7 +751,7 @@ def convert_links(text):
         # Check for common image file extensions
         if any(url.lower().endswith(ext) for ext in ['.jpg', '.jpeg', '.png', '.gif']):
             # Extracted inventory page URL for the current image
-            inventory_page_url = extract_inventory_page_url(text, alt_or_text)
+            inventory_page_url = extract_inventory_page_urls(text)
             if inventory_page_url:
                 return f'<a href="{inventory_page_url}" target="_blank"><img src="{url}" alt="{alt_or_text}" style="width: 100px; height: auto;"/></a>'
             else:
